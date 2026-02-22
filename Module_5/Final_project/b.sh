0784be9d52622ed96197384bf1532a86cca3ea2a
Ivan Chetverkin <104260618+IvanChet-4@users.noreply.github.com>
2026-02-22 15:54:57 +0300

    Update b.sh

--- Module_5/Final_project/b.sh
+++ Module_5/Final_project/b.sh
@@ -8,3 +8,24 @@ mapfile -t CURRENT_PIDS < <(
 printf '%s\n' "${CURRENT_PIDS[@]}"
 
 ====================================================
+
+#!/usr/bin/env bash
+
+# Считываем список PID
+mapfile -t CURRENT_PIDS < <(
+  find /proc -maxdepth 1 -type d -regex '.*/[0-9]+' -printf '%f\n'
+)
+
+# Выводим PID и соответствующий exe
+for pid in "${CURRENT_PIDS[@]}"; do
+  exe_path="/proc/$pid/exe"
+  if [[ -r "$exe_path" ]]; then
+    # readlink разрешает симлинк
+    exe_name=$(readlink -f "$exe_path")
+  else
+    exe_name="<нет доступа>"
+  fi
+  printf '%-6s %s\n' "$pid" "$exe_name"
+done
+
+====================================================
