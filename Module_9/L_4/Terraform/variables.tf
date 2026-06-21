ab0286b44816c74c234cec64f6b022f77e6d2426
Ivan Chetverkin <104260618+IvanChet-4@users.noreply.github.com>
2026-06-21 19:28:00 +0300

    Create variables.tf

---
+++ Module_9/L_4/Terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "yandex_cloud_token" {
+  description = "Yandex Cloud IAM token"
+  type        = string
+}
+
+variable "cloud_id" {
+  description = "Yandex Cloud ID"
+  type        = string
+}
+
+variable "folder_id" {
+  description = "Yandex Folder ID"
+  type        = string
+}
