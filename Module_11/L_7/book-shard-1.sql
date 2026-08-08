1133daca3b2a06acb36a25f3f133779bf2ae6a3f
Ivan Chetverkin <104260618+IvanChet-4@users.noreply.github.com>
2026-08-08 17:00:23 +0300

    Create book-shard-1.sql

---
+++ Module_11/L_7/book-shard-1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE books_shard_1 (
+    book_id BIGINT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(150) NOT NULL,
+    isbn VARCHAR(13) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT NOT NULL DEFAULT 0
+);
