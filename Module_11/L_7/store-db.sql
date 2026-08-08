e7fb7ae0b54680fed9c0540ab3a589582ed4f96c
Ivan Chetverkin <104260618+IvanChet-4@users.noreply.github.com>
2026-08-08 17:03:31 +0300

    Create store-db.sql

---
+++ Module_11/L_7/store-db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE stores_main (
+    store_id INT PRIMARY KEY,
+    store_name VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    manager_name VARCHAR(100) NOT NULL
+);
